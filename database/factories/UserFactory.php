51d921f58c4dffcc324eeae7ef287ae519aa3581
DanielMikolajczyk <56119744+DanielMikolajczyk@users.noreply.github.com>
2021-08-28 08:42:23 +0200

    Admin user seeder

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -9,46 +9,46 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 
 class UserFactory extends Factory
 {
-    /**
-     * The name of the factory's corresponding model.
-     *
-     * @var string
-     */
-    protected $model = User::class;
+  /**
+   * The name of the factory's corresponding model.
+   *
+   * @var string
+   */
+  protected $model = User::class;
 
-    /**
-     * Define the model's default state.
-     *
-     * @return array
-     */
-    public function definition()
-    {
-      $departaments = Departament::all();
-      $roles = Role::all();
+  /**
+   * Define the model's default state.
+   *
+   * @return array
+   */
+  public function definition()
+  {
+    $departaments = Departament::all();
+    $roles = Role::all();
 
-        return [
-            'name' => $this->faker->name(),
-            'email' => $this->faker->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'phone_number' => $this->faker->phoneNumber(),
-            'departament_id' => $departaments->random()->id,
-            'actual_departament' => $departaments->random()->id,
-            'role_id' => $roles->random()->id,
-        ];
-    }
+    return [
+      'name' => $this->faker->name(),
+      'email' => $this->faker->unique()->safeEmail(),
+      'email_verified_at' => now(),
+      'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+      'phone_number' => $this->faker->phoneNumber(),
+      'departament_id' => $departaments->random()->id,
+      'actual_departament' => $departaments->random()->id,
+      'role_id' => $roles->random()->id,
+    ];
+  }
 
-    /**
-     * Indicate that the model's email address should be unverified.
-     *
-     * @return \Illuminate\Database\Eloquent\Factories\Factory
-     */
-    public function unverified()
-    {
-        return $this->state(function (array $attributes) {
-            return [
-                'email_verified_at' => null,
-            ];
-        });
-    }
+  /**
+   * Indicate that the model's email address should be unverified.
+   *
+   * @return \Illuminate\Database\Eloquent\Factories\Factory
+   */
+  public function unverified()
+  {
+    return $this->state(function (array $attributes) {
+      return [
+        'email_verified_at' => null,
+      ];
+    });
+  }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -7,13 +7,22 @@ use Illuminate\Database\Seeder;
 
 class UserSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        User::factory(10)->create();
-    }
+  /**
+   * Run the database seeds.
+   */
+  public function run(): void
+  {
+    User::create([
+      'name' => 'Admin',
+      'email' => 'admin@admin.com',
+      'email_verified_at' => now(),
+      'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+      'phone_number' => '123456789',
+      'departament_id' => 1,
+      'actual_departament' => 1,
+      'role_id' => 1,
+    ]);
+    
+    User::factory(10)->create();
+  }
 }
